212f8beaaf951ded49f479fba995d0b4fb871fe2
Silvia Pineda Rios <silvy@MacBook-Air-de-Silvia.local>
2024-03-23 12:44:25 -0500

    subiendo actualización de la función renderItems

--- src/view.js
+++ src/view.js
@@ -1,15 +1,25 @@
 export const renderItems = (data) => {
-  
   // Crear un elemento <ul>
   const ulElement = document.createElement('ul');
-
+  
   // Recorrer la data
   data.forEach(item => {
     // Crear un elemento <li> para cada item
     const liElement = document.createElement('li');
+    const liImg = document.createElement('img');
+    const liDescription = document.createElement('li');
 
     // Asignar el contenido del item al <li>
-    liElement.textContent = "jajaja";
+    liImg.src = item.imageUrl;
+    liElement.textContent = item.name;
+    liDescription.textContent = item.shortDescription;
+
+    // Agregar el atributo itemtype al <li>
+    liElement.setAttribute('itemtype');
+
+    // Agregar el <img> y los <li> al <li> principal
+    liElement.appendChild(liImg);
+    liElement.appendChild(liDescription);
 
     // Agregar el <li> al <ul>
     ulElement.appendChild(liElement);
@@ -18,4 +28,6 @@ export const renderItems = (data) => {
   // Retornar el elemento <ul>
   return ulElement;
 };
+
+
 
