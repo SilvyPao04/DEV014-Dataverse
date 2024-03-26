2c161d289e57838bae506ba73682798ffca27fc9
Silvia Pineda Rios <silvy@MacBook-Air-de-Silvia.local>
2024-03-26 13:52:07 -0500

    actualizacion de función renderItems con tests en ok - Hito2

--- src/view.js
+++ src/view.js
@@ -1,30 +1,35 @@
 export const renderItems = (data) => {
-  
   // Crear un elemento <ul>
-  
-  const divElement = document.createElement('div');
-  
+  const ulElement = document.createElement('ul');
+
   // Recorrer la data
   data.forEach(item => {
     // Crear un elemento <li> para cada item
-    const liImg = document.createElement('img');
     const liElement = document.createElement('li');
-    const liDescription = document.createElement('li')
+    const liImg = document.createElement('img');
+    const liDescription = document.createElement('p');
+
+    //Asignar la clase item al <li>
+    liElement.classList.add("item");
 
     // Asignar el contenido del item al <li>
     liImg.src = item.imageUrl;
     liElement.textContent = item.name;
     liDescription.textContent = item.shortDescription;
 
+    // Asignar el atributo itemtype a los elementos <li>
+    liElement.setAttribute("itemtype", "actors");
+    liElement.setAttribute("itemscope","")
+    
+    // Agregar la imagen y la descripción como hijos de <li>
+    liElement.appendChild(liImg);
+    liElement.appendChild(liDescription);
+
     // Agregar el <li> al <ul>
-    divElement.appendChild(liImg);
-    divElement.appendChild(liElement);
-    divElement.appendChild(liDescription);
+    ulElement.appendChild(liElement);
   });
 
+
   // Retornar el elemento <ul>
-  return divElement;
+  return ulElement;
 };
-
-
-
