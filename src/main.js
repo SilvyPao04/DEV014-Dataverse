8d4168fac6c772a4d5f629aacbcb8b707b918939
Malu <72827072+maluhuillcaelguera@users.noreply.github.com>
2024-03-27 11:44:12 -0500

    Update main.js
    subiendo mein

--- src/main.js
+++ src/main.js
@@ -1,23 +1,9 @@
 import { example } from './dataFunctions.js';
 import { renderItems } from './view.js';
 import data from './data/dataset.js';
-
 console.log(example, renderItems(data), data);
-
 const itemsList = renderItems(data); // creamos una constante para guardar los datos en una lista e invocar sólo una 1 vez renderItems para no duplicar datos
-
 // tiene que ser un sólo contenedor para todos los 24 elementos para no duplicarlos
 const container = document.querySelector('#root');
-
 container.appendChild(itemsList.cloneNode(true));
-
-
-//const fotos = document.getElementById('fotos');
-const container = document.getElementById('root');
-//const description = document.getElementById('descripcion');
-
-
-//fotos.appendChild(itemsList.cloneNode(true)); 
-container.appendChild(itemsList.cloneNode(true));
-//description.appendChild(itemsList.cloneNode(true));
 
