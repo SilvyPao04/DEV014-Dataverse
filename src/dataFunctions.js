da2127d62b0f09d2aa957ae90f4b993fc987aa9e
maluhuillcaelguera <maluhuillcaelguera@gmail.com>
2024-03-28 12:06:12 -0500

    subiendo funcion de filtrar datos

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,15 +1,30 @@
 // Estas funciones son ejemplos, aquí puedes desarrollar tus propias funciones.
 
-export const example = () => {
-  return 'example';
-};
+//import dataset from "./data/dataset";
 
-export const anotherExample = () => {
-  return [];
-};
+export const filterData = (data, filterBy, value) => {
+  let  resultadoFiltro = []
+  resultadoFiltro = data.filter((element)=>{
+    
+    return element.facts[filterBy] === value
+  })
 
+  return resultadoFiltro
+}
+
+export const filterData2 = (data, filterBy, value) => {
+  let  resultadoFiltro2 = []
+  resultadoFiltro2 = data.filter((element)=>{
+    
+    return element.facts[filterBy] <= value 
+  })
+
+  return resultadoFiltro2
+}
+
+// Restablecer los valores predeterminados de los selectores
 document.getElementById('limpiar').addEventListener('click', function() {
-  // Restablecer los valores predeterminados de los selectores
+  
   document.getElementById('filtro1').selectedIndex = 0;
   document.getElementById('filtro2').selectedIndex = 0;
   document.getElementById('filtro3').selectedIndex = 0;
--- src/main.js
+++ src/main.js
@@ -1,9 +1,60 @@
-import { example } from './dataFunctions.js';
+import { filterData} from './dataFunctions.js';
+import { filterData2} from './dataFunctions.js';
 import { renderItems } from './view.js';
 import data from './data/dataset.js';
-console.log(example, renderItems(data), data);
+
 const itemsList = renderItems(data); // creamos una constante para guardar los datos en una lista e invocar sólo una 1 vez renderItems para no duplicar datos
 // tiene que ser un sólo contenedor para todos los 24 elementos para no duplicarlos
 const container = document.querySelector('#root');
 container.appendChild(itemsList.cloneNode(true));
+
+// Selecciona el elemento <select> por su ID
+const selector = document.getElementById("filtro1");
+const selector2 = document.getElementById("filtro2");
+
+// Agrega un evento de cambio al elemento <select>
+selector.addEventListener("change", function() {
+// Captura el valor seleccionado
+  const valorSeleccionado = selector.value;
+  const valorSeleccionado2 = selector2.value;
+  
+  // Filtra los datos según el valor seleccionado raza
+  const datosFiltrados = filterData(data, 'race', valorSeleccionado);
+  // Filtra los datos según el valor seleccionado raza
+
+  console.log( filterData2(data, 'age', valorSeleccionado2))
+  // Elimina los elementos anteriores del contenedor
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  // Renderiza los elementos filtrados en el contenedor
+  const itemsFiltradoraza = renderItems(datosFiltrados);
+  container.appendChild(itemsFiltradoraza);
+
+});
+
+
+// Agrega un evento de cambio al elemento <select>
+selector2.addEventListener("change", function() {
+    // Captura el valor seleccionado
+   
+      const valorSeleccionado2 = selector2.value;
+
+      // Filtra los datos según el valor seleccionado edad
+      const datosFiltrados2 = filterData2(data, 'age', valorSeleccionado2);
+    
+      console.log( filterData2(data, 'age', valorSeleccionado2))
+      // Elimina los elementos anteriores del contenedor
+      while (container.firstChild) {
+        container.removeChild(container.firstChild);
+      }
+    
+      // Renderiza los elementos filtrados en el contenedor
+      const itemsFiltradoedad = renderItems(datosFiltrados2);
+      container.appendChild(itemsFiltradoedad);
+    
+      
+      
+    });
+
 
